79233575eee8637be13a53809a4200046e54a2db
fernando <fernando23296@gmail.com>
2021-05-24 23:51:59 -0400

    First edition of web

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/cv_1_c1')
+def cv_1_c1():
+    return render_template('cv_1_c1.html')
+
+@app.route('/cv_1_c2')
+def cv_1_c2():
+    return render_template('cv_1_c2.html')
+
+@app.route('/cv_1_c3')
+def cv_1_c3():
+    return render_template('cv_1_c3.html')
+
+@app.route('/intro_ia')
+def intro_ia():
+    return render_template('intro_ia.html')
+
+
+@app.route('/ia_c1')
+def ia_c1():
+    return render_template('ia_c1.html')
+
+@app.route('/ia_c2')
+def ia_c2():
+    return render_template('ia_c2.html')
+
+@app.route('/ia_c3')
+def ia_c3():
+    return render_template('ia_c3.html')
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
